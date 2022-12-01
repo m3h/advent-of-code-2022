de6a495d5c9ff94e271015f3540820e804e94341
Reece Volker <Reece.Volker@partner.bmw.co.za>
2022-12-01 07:39:47 +0200

    day 1

---
+++ src/day1p2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+
+def read_input(fpath):
+    with open(fpath) as f:
+        return f.readlines()
+
+
+def main(fpath: str):
+    data = read_input(fpath)
+
+    elves_calories = [0]
+
+    for item in data:
+        if item.strip() == "":
+            elves_calories.append(0)
+        else:
+            elves_calories[-1] += int(item)
+
+    print(sorted(elves_calories, reverse=True)[:3])
+    print(sum(sorted(elves_calories, reverse=True)[:3]))
+
+
+if __name__ == "__main__":
+
+    input_type = "test"
+    input_type = "full"
+    fpath = f"./input/{input_type}/day1/input.txt"
+
+    main(fpath)
